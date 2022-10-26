1f81b4598826f23626aa073b38d7621003df7385
Bruno Gonçalves <brunovg@gmail.com>
2022-10-26 18:42:59 +0100

    added zip and unzip block

--- utils/src/main/java/org/assimbly/util/DependencyUtil.java
+++ utils/src/main/java/org/assimbly/util/DependencyUtil.java
@@ -91,7 +91,6 @@ public class DependencyUtil {
         AGGREGATE("aggregate"),
         AMAZON("amazon"),
         APPENDTOBODY("appendtobody"),
-        ARCHIVE("archive"),
         BASE64TOTEXT("base64totext"),
         CONTINUEFLOW("continueflow"),
         CSVTOXML("csvtoxml"),
@@ -129,12 +128,14 @@ public class DependencyUtil {
         SPLIT("split"),
         TEXTTOBASE64("texttobase64"),
         THROTTLE("throttle"),
+        UNZIP("unzip"),
         VELOCITY("velocity"),
         WIRETAP("wiretap"),
         XMLTOEDI("xmltoedi"),
         XMLTOEDIFACT("xmltoedifact"),
         XMLTOJSON("xmltojson"),
         XSLT("xslt"),
+        ZIP("zip"),
         ;
 
         private static Map<String, PredefinedBlocks> BY_LABEL = new HashMap<>();
